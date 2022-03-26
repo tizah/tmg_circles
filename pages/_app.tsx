daf9e6c570882c994cf780dff3dd6924a4f3333f
tizah <davidzagi@yahoo.com>
2022-03-26 15:22:01 +0100

    added url callback for auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,16 @@ import '../components/Header/Header.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <Auth0Provider
+  //   <Auth0Provider
+  //   domain="dev-wgu9aun9.auth0.com"
+  //   clientId="sA06YGcCapyqJImzwv4tUQRVg6VlDBX3"
+  //   redirectUri={`http://localhost:3000`}
+  // >
+
+  <Auth0Provider
     domain="dev-wgu9aun9.auth0.com"
-    clientId="sA06YGcCapyqJImzwv4tUQRVg6VlDBX3"
-    redirectUri={`http://localhost:3000`}
+    clientId="BT0dXeyhwfwI7mB5wnjHo0PWX4JK359M"
+    redirectUri={`https://tmgcircles.netlify.app/`}
   >
       <Component {...pageProps} />
   </Auth0Provider>
